0cbb78cc252e16116aa344ed34b66a440ad26259
ayan412 <mussatayev.ayan@gmail.com>
2023-02-01 08:57:43 +0600

    closures

---
+++ Basic/Lesson5/FuncClosures.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func totalPrice(initPrice int) func(int) int {
+	sum := initPrice // часть внешней ф-й totalPrice
+	return func(x int) int { // anonymous function which generate closures
+		sum += x
+		return sum // sum возвращается в sum на 4-й строке или нет?
+	}
+}
+
+func main() {
+	orderPrice := totalPrice(1) // ч-з присваивание orderPrice стала ф-й к-я сама принимает и возвр-т число, поэтому при ее вызове ей перед-я число
+	fmt.Println(orderPrice(1)) // поэтому при ее вызове ей передается число
+	fmt.Println(orderPrice(1))
+	fmt.Println(orderPrice(1))
+	fmt.Println(orderPrice(1))
+}
